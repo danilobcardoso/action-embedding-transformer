9c4846a9074c981e742c83a969a3d9733a4eeaed
Danilo Cardoso <danilo.cardoso@dcc.ufmg.br>
2021-02-26 10:36:57 -0300

    Adicionando positional encoding

--- layers.py
+++ layers.py
@@ -2,6 +2,7 @@ import math, copy, time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 import numpy as np
 
 def clones(module, N):
@@ -129,6 +130,7 @@ class TemporalSelfAttention(nn.Module):
         x = x.view(n, t, -1) # [N, T, V*Cout, H] -> [N, T, V*Cout*H]
         return x
 
+
 class TemporalInputAttention(nn.Module):
     def __init__(self, heads, embedding_in, embedding_out, memory_in):
         super().__init__()
@@ -154,3 +156,24 @@ class TemporalInputAttention(nn.Module):
         x = x.permute(0, 2, 3, 1).contiguous() # [N, H, T, V*Cout] -> [N, T, V*Cout, H]
         x = x.view(n, t, -1) # [N, T, V*Cout, H] -> [N, T, V*Cout*H]
         return x
+
+class PositionalEncoding(nn.Module):
+    "Implement the PE function."
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) *
+                             -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)],
+                         requires_grad=False)
+        return self.dropout(x)
--- zoo_pose_embedding.py
+++ zoo_pose_embedding.py
@@ -4,13 +4,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-from layers import SpatialGCN, to_embedding_form, to_gcn_layer, from_gcn_layer
+from layers import SpatialGCN, PositionalEncoding, to_embedding_form, to_gcn_layer, from_gcn_layer
 
 class TwoLayersGCNPoseEmbedding(nn.Module):
-    def __init__(self, input_channel, output_channel, kernel_size):
+    def __init__(self, input_channel, output_channel, kernel_size, num_nodes=25, dropout=0.1):
         super().__init__()
         self.spatial_gcn_1 = SpatialGCN(input_channel, 12, kernel_size)
         self.spatial_gcn_2 = SpatialGCN(12, output_channel, kernel_size)
+
+        self.positional_encoding = PositionalEncoding(output_channel*num_nodes, dropout )
 
     '''
         # [N, T, V, Ci] -> [N, T, V*Co]
@@ -26,4 +28,5 @@ class TwoLayersGCNPoseEmbedding(nn.Module):
         x = from_gcn_layer(x, num_nodes=v) # [ N, Cout, T, V] -> [ N, T, V, Cout]
         x = to_embedding_form(x) #  [ N, T, V, Cout] ->  [ N, T, V * Cout]
 
+        x = self.positional_encoding(x)
         return x
