a0afaed224a40982505155eed919a14b607f01cf
Danilo Cardoso <danilo.cardoso@dcc.ufmg.br>
2021-03-10 16:10:34 -0300

    treinamento

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from render import animate, save_animation
 from datasets import NTUDataset
 
 # Model components
-from zoo_pose_embedding import TwoLayersGCNPoseEmbedding
+from zoo_pose_embedding import TwoLayersGCNPoseEmbedding, JoaosDownsampling
 from zoo_action_encoder_units import TransformerEncoderUnit
 from zoo_action_decoder_units import TransformerDecoderUnit
 from zoo_upsampling import StepByStepUpsampling, JoaosUpsampling
@@ -36,17 +36,18 @@ adjacency = Graph(ntu_rgbd)
 conf_kernel_size = adjacency.A.shape[0]
 conf_num_nodes = adjacency.A.shape[1]
 conf_heads = 5
-conf_encoding_per_node = 20
+conf_encoding_per_node = 50
 conf_internal_per_node = int(conf_encoding_per_node/conf_heads)
 
 class BetterThatBestModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = ActionEmbeddingTransformer(
-            TwoLayersGCNPoseEmbedding(
-                3,
-                conf_encoding_per_node,
-                conf_kernel_size
+            JoaosDownsampling(
+                conf_num_nodes,
+                conf_encoding_per_node*conf_num_nodes,
+                node_channel_in = 3,
+                device=device
             ),
             TransformerEncoderUnit (
                 heads=conf_heads,
@@ -83,11 +84,11 @@ for p in model.parameters():
 
 criterion = torch.nn.MSELoss()
 
-#ntu_dataset = NTUDataset(root_dir='../ntu-rgbd-dataset/Python/raw_npy/')
-ntu_dataset = NTUDataset(root_dir='../datasets/NTURGB-D/Python/raw_npy/')
+ntu_dataset = NTUDataset(root_dir='../ntu-rgbd-dataset/data/raw_npy/')
+# ntu_dataset = NTUDataset(root_dir='../datasets/NTURGB-D/Python/raw_npy/')
 loader = DataLoader(ntu_dataset, batch_size=512, shuffle=True)
 #optimizer = torch.optim.SGD(model.parameters(), lr=0.00005)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.000005)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 
@@ -98,7 +99,7 @@ for epoch in range(20):
     for data in pbar:
         data = data.to(device, dtype=torch.float)
 
-        n_out, c_out, t_out, v_out = data.size()
+        n_out, t_out, v_out, c_out = data.size()
         mask = subsequent_mask(t_out).to(device, dtype=torch.float)
 
         out = model(data, data, A, mask)
@@ -112,5 +113,5 @@ for epoch in range(20):
 
     print('Epoch {} loss = {}'.format(epoch, loss.item()))
     torch.save(model.state_dict(), 'outputs/models/simple_encoder_epoch_{}.pth'.format(epoch))
-    save_animation(data[0], ntu_rgbd, 'outputs/animations/output_example_epoch_{}.gif'.format(epoch))
-    save_animation(out[0], ntu_rgbd, 'outputs/animations/sample_example_epoch_{}.gif'.format(epoch))
+    # save_animation(data[0], ntu_rgbd, 'outputs/animations/output_example_epoch_{}.gif'.format(epoch))
+    # save_animation(out[0], ntu_rgbd, 'outputs/animations/sample_example_epoch_{}.gif'.format(epoch))
